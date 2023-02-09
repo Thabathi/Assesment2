2a70c4a662cc0669e6a25dc412797e3d45bcd26a
Thabathi <123152978+Thabathi@users.noreply.github.com>
2023-02-09 18:36:48 +0530

    Update Insertion.java

--- Insertion.java
+++ Insertion.java
@@ -1,31 +1,39 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-	public class Insertion {
+public class Question_8 {
 
-		public static void main(String[] args) {
-
-			int n, i, j, element;
-			Scanner s = new Scanner(System.in);
-			System.out.println("enter the size of the array:");
-			n = s.nextInt();
-			int[] arr = new int[n];
-			System.out.println("enter" + n + "Elements:");
-			for (i = 0; i < n; i++)
-				arr[i] = s.nextInt();
-			for (i = 1; i < n; i++)
-			{
-				element = arr[i];
-				for (j = (i - 1); j >= 0 && arr[j] > element; j--)
-					arr[j + 1] = arr[j];
-				arr[j + 1] = element;
-			}
-			System.out.println("The new sorted array is:");
-			for (i = 0; i < n; i++)
-				System.out.print(arr[i] + " ");
-
+    public static void main(String[] args) {
+           	Scanner s = new Scanner(System.in);
+		System.out.println("enter the size of the array:");
+		int n = s.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter elements:");
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = s.nextInt();
+		    
 		}
-	}
 
+        int len = arr.length;
 
+        for(int i=0;i<len-1;i++) {
+
+            for(int j=i+1;j<len;j++) {
+
+                if(arr[j] < arr[i]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        System.out.print("Insertion sort array: ");
+        for(int i=0;i<len;i++) {
+            System.out.print(arr[i]+ " ");
+        }
+
+    }
+
+}
 
