f5e5388b58ed90a891b9c2f799b6c762db9c5ef3
Thabathi <123152978+Thabathi@users.noreply.github.com>
2023-02-09 18:40:59 +0530

    Add files via upload

---
+++ Question_7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Question_7 {
+
+	public static void main(String[] args) {
+		 Scanner s=new Scanner(System.in);
+		 System.out.println("Enter the input element:");
+		 String input=s.nextLine();
+		 System.out.println("{}");
+		  {
+		 
+				 
+
+	}
+
+}}
